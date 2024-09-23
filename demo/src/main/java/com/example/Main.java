0659a88d591530b9e4a523f91535c231e46496e5
SanobarEnigma <sanobarfatima96@gmail.com>
2024-09-23 18:52:04 +0530

    branch2

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -2,10 +2,10 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello ABC!");
+        System.out.println("Hello XYZ!");
     }
 
     public static void printMessage() {
-        System.out.println("You are in feature one");
+        System.out.println("You are in feature two");
     }
 }
